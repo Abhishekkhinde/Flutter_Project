63a5f433f3b4763a8802e1ef88d6f64874dcb08c
Abhishek Khinde <abhisheknkhinde11@gmail.com>
2024-03-07 00:42:29 +0530

    daily Flash

---
+++ advanced_todo_list/README.md
@@ -0,0 +1,3 @@
+# advanced_todo_list
+
+A new Flutter project.
---
+++ advanced_todo_list/lib/todoapp.dart
@@ -0,0 +1,346 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class ToDoApp extends StatefulWidget {
+  const ToDoApp({super.key});
+
+  @override
+  State createState() => _ToDoAppState();
+}
+
+class ToDoModelClass {
+  String? title;
+  String? description;
+  String? date;
+
+  ToDoModelClass({this.title, this.description, this.date});
+}
+
+class _ToDoAppState extends State {
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
+  TextEditingController dateController = TextEditingController();
+
+  List todolist = [
+    ToDoModelClass(
+        title: "todo list", description: "abhsihek khinde ", date: "12-10-23"),
+    ToDoModelClass(
+        title: "todo ", description: "abhsihek ke ", date: "12-10-24"),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromRGBO(111, 81, 255, 1),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(
+            height: 45,
+          ),
+          Container(
+            padding: const EdgeInsets.only(left: 29),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Good Morning",
+                  style: GoogleFonts.quicksand(
+                      fontWeight: FontWeight.w400,
+                      fontSize: 22,
+                      color: const Color.fromRGBO(255, 255, 255, 1)),
+                ),
+                Text(
+                  "Abhishek",
+                  style: GoogleFonts.quicksand(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 30,
+                      color: const Color.fromRGBO(255, 255, 255, 1)),
+                ),
+                const SizedBox(
+                  height: 45,
+                )
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Color.fromRGBO(217, 217, 217, 1),
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(40),
+                  topRight: Radius.circular(40),
+                ),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.only(
+                      top: 19,
+                      bottom: 19,
+                    ),
+                    child: Text(
+                      "CREATE TO DO LIST",
+                      style: GoogleFonts.quicksand(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 12,
+                          color: const Color.fromRGBO(0, 0, 0, 1)),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        color: Color.fromRGBO(255, 255, 255, 1),
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(40),
+                          topRight: Radius.circular(40),
+                        ),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                          top: 39,
+                        ),
+                        child: ListView.builder(
+                          itemCount: todolist.length,
+                          itemBuilder: (context, index) {
+                            return Padding(
+                              padding:
+                                  const EdgeInsets.only(top: 10, bottom: 10),
+                              child: Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child: Row(
+                                  // mainAxisAlignment: MainAxisAlignment.start,
+                                  // crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Container(
+                                      height: 52,
+                                      width: 52,
+                                      decoration: const BoxDecoration(
+                                          shape: BoxShape.circle,
+                                          color:
+                                              Color.fromRGBO(217, 217, 217, 1)),
+                                      child: Image.asset("assets/Group.png"),
+                                    ),
+                                    const SizedBox(
+                                      width: 20,
+                                    ),
+                                    Expanded(
+                                      child: Column(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.start,
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Text(
+                                            todolist[index].title,
+                                            style: GoogleFonts.inter(
+                                              fontSize: 11,
+                                              fontWeight: FontWeight.w500,
+                                              color: const Color.fromRGBO(
+                                                  0, 0, 0, 1),
+                                            ),
+                                          ),
+                                          const SizedBox(
+                                            height: 8,
+                                          ),
+                                          Text(
+                                            todolist[index].title,
+                                            style: GoogleFonts.inter(
+                                              fontSize: 9,
+                                              fontWeight: FontWeight.w400,
+                                              color: const Color.fromRGBO(
+                                                  0, 0, 0, 1),
+                                            ),
+                                          ),
+                                          const SizedBox(
+                                            height: 2,
+                                          ),
+                                          Text(
+                                            todolist[index].date,
+                                            style: GoogleFonts.inter(
+                                              fontSize: 8,
+                                              fontWeight: FontWeight.w500,
+                                              color: const Color.fromRGBO(
+                                                  0, 0, 0, 1),
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                    GestureDetector(
+                                      child: Container(
+                                        height: 15,
+                                        width: 15,
+                                        decoration: const BoxDecoration(
+                                          shape: BoxShape.circle,
+                                        ),
+                                        child: const Icon(Icons.home,
+                                            color: Colors.black, size: 15),
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: () {
+        showModalBottomSheet(
+          isScrollControlled: true,
+          context: context,
+          builder: (context) {
+            return Padding(
+              padding: EdgeInsets.only(
+                top: 13,
+                left: 15,
+                right: 15,
+                bottom: MediaQuery.of(context).viewInsets.bottom,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 26),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    Text(
+                      "Create Task",
+                      style: GoogleFonts.quicksand(
+                        fontSize: 22,
+                        fontWeight: FontWeight.w600,
+                        color: const Color.fromRGBO(0, 0, 0, 1),
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Title",
+                          style: GoogleFonts.quicksand(
+                            fontSize: 11,
+                            fontWeight: FontWeight.w400,
+                            color: const Color.fromRGBO(0, 0, 0, 1),
+                          ),
+                        ),
+                        TextField(
+                          controller: titleController,
+                          decoration: InputDecoration(
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(12),
+                              borderSide: const BorderSide(
+                                color: Color.fromRGBO(89, 57, 241, 1),
+                              ),
+                            ),
+                            border: OutlineInputBorder(
+                              borderSide: const BorderSide(
+                                color: Color.fromRGBO(89, 57, 241, 1),
+                              ),
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          "Description",
+                          style: GoogleFonts.quicksand(
+                            fontSize: 11,
+                            fontWeight: FontWeight.w400,
+                            color: const Color.fromRGBO(0, 0, 0, 1),
+                          ),
+                        ),
+                        TextField(
+                          controller: descriptionController,
+                          decoration: InputDecoration(
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(12),
+                              borderSide: const BorderSide(
+                                color: Color.fromRGBO(89, 57, 241, 1),
+                              ),
+                            ),
+                            border: OutlineInputBorder(
+                              borderSide: const BorderSide(
+                                color: Color.fromRGBO(89, 57, 241, 1),
+                              ),
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          "Date",
+                          style: GoogleFonts.quicksand(
+                            fontSize: 11,
+                            fontWeight: FontWeight.w400,
+                            color: const Color.fromRGBO(0, 0, 0, 1),
+                          ),
+                        ),
+                        TextField(
+                          controller: dateController,
+                          decoration: InputDecoration(
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(12),
+                              borderSide: const BorderSide(
+                                color: Color.fromRGBO(89, 57, 241, 1),
+                              ),
+                            ),
+                            border: OutlineInputBorder(
+                              borderSide: const BorderSide(
+                                color: Color.fromRGBO(89, 57, 241, 1),
+                              ),
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                      ],
+                    ),
+                    Container(
+                      height: 50,
+                      width: 300,
+                      decoration: const BoxDecoration(
+                        color: Color.fromRGBO(89, 57, 241, 1),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(20),
+                        ),
+                      ),
+                      child: Center(
+                        child: Text(
+                          "submit",
+                          style: GoogleFonts.inter(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w700,
+                            color: const Color.fromRGBO(255, 255, 255, 1),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            );
+          },
+        );
+      }),
+    );
+  }
+}
