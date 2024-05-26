2d19321dca542747a48bf66ebe5b38296f15f246
Abhishek Khinde <abhisheknkhinde11@gmail.com>
2024-05-26 17:22:56 +0530

    Plant App

--- plant_app/lib/main.dart
+++ plant_app/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:plant_app/getstartpage.dart';
+import 'package:plant_app/view/getstartpage.dart';
 
 void main() {
   runApp(const MainApp());
