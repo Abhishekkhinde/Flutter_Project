f6d916fba6c61cbea7779a6e4ac6b90e59aaebf2
Abhishek Khinde <abhisheknkhinde11@gmail.com>
2024-06-18 15:36:49 +0530

    Brand Store App

--- brand_store_app/lib/view/cart_page.dart
+++ brand_store_app/lib/view/cart_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class CartScreen extends StatefulWidget {
@@ -12,29 +13,136 @@ class _CartScreenState extends State {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          leading: IconButton(
-            onPressed: () {},
-            icon: const Image(
-              image: AssetImage("assets/icon/back.png"),
-              color: Color.fromRGBO(13, 13, 14, 1),
-              fit: BoxFit.fill,
-              height: 20,
-              width: 11,
-            ),
-          ),
-          centerTitle: true,
-          title: Text(
-            "Checkout",
-            style: GoogleFonts.imprima(
-                fontWeight: FontWeight.w400,
-                fontSize: 18,
-                color: const Color.fromRGBO(13, 13, 14, 1)),
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Image(
+            image: AssetImage("assets/icon/back.png"),
+            color: Color.fromRGBO(13, 13, 14, 1),
+            fit: BoxFit.fill,
+            height: 20,
+            width: 11,
           ),
         ),
-        body: Padding(
-            padding: const EdgeInsets.all(30),
-            child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start, children: [])));
+        centerTitle: true,
+        title: Text(
+          "Checkout",
+          style: GoogleFonts.imprima(
+              fontWeight: FontWeight.w400,
+              fontSize: 18,
+              color: const Color.fromRGBO(13, 13, 14, 1)),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(30),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          Text(
+            "My Orders",
+            style: GoogleFonts.imprima(
+              fontWeight: FontWeight.w500,
+              fontSize: 40,
+              color: const Color.fromRGBO(13, 13, 14, 1),
+            ),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Container(
+            child: Row(
+              children: [
+                Container(
+                  margin: EdgeInsets.only(right: 22),
+                  height: 150,
+                  width: 108,
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(16),
+                    ),
+                    image: DecorationImage(
+                        image: AssetImage("assets/homeimage/home1.png"),
+                        fit: BoxFit.fill),
+                  ),
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Premium Tagerine Shirt",
+                        style: GoogleFonts.imprima(
+                          fontWeight: FontWeight.w400,
+                          fontSize: 18,
+                          color: const Color.fromRGBO(13, 13, 14, 1),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 16,
+                      ),
+                      Text(
+                        "Yellow",
+                        style: GoogleFonts.imprima(
+                          fontWeight: FontWeight.w400,
+                          fontSize: 14,
+                          color: const Color.fromRGBO(121, 119, 128, 1),
+                        ),
+                      ),
+                      Text(
+                        "Size 8",
+                        style: GoogleFonts.imprima(
+                          fontWeight: FontWeight.w400,
+                          fontSize: 14,
+                          color: const Color.fromRGBO(121, 119, 128, 1),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 14,
+                      ),
+                      Text(
+                        "\$257.85",
+                        style: GoogleFonts.imprima(
+                          fontWeight: FontWeight.w400,
+                          fontSize: 26,
+                          color: const Color.fromRGBO(13, 13, 14, 1),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  width: 22,
+                ),
+                Column(
+                  children: [
+                    SizedBox(
+                      height: 125,
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          "1",
+                          style: GoogleFonts.imprima(
+                            fontWeight: FontWeight.w400,
+                            fontSize: 32,
+                            color: const Color.fromRGBO(13, 13, 14, 1),
+                          ),
+                        ),
+                        Text(
+                          "x",
+                          style: GoogleFonts.imprima(
+                            fontWeight: FontWeight.w400,
+                            fontSize: 20,
+                            color: const Color.fromRGBO(13, 13, 14, 1),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                )
+              ],
+            ),
+          )
+        ]),
+      ),
+    );
   }
 }
