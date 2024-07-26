9d5f67231f4bba744ad050ea21d5b8e41b699527
Abhishek Khinde <abhisheknkhinde11@gmail.com>
2024-07-26 18:09:32 +0530

    Music App

--- music_app/lib/view/cart_page.dart
+++ music_app/lib/view/cart_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class CartScreen extends StatefulWidget {
   const CartScreen({super.key});
@@ -10,7 +11,26 @@ class _CartScreenState extends State<CartScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("cart"),
+      backgroundColor: const Color.fromRGBO(24, 24, 24, 1),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 30, left: 10, right: 10),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Cart",
+                textAlign: TextAlign.center,
+                style: GoogleFonts.inter(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 24,
+                  color: const Color.fromRGBO(255, 46, 0, 1),
+                ),
+              ),
+            ],
+          ),
+        ]),
+      ),
     );
   }
 }
